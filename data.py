26a01f43ec74427df596cb3306a67f9e44757c2b
dev-sereti <devsereti@gmail.com>
2025-09-28 12:46:49 +0300

    DUPLICATES

--- data.py
+++ data.py
@@ -1,24 +1,8 @@
-#Creating Series and Data Frames
 import pandas as pd
-data=pd.Series([10,20,30,40],index=['A','B','C','D'])
-df=pd.DataFrame({
-    'Name':['Alice','Bob','Charlie','Kelvin'],
-    'Age':[21,22,23,34],
-    'Score':[33,45,55,67]
+
+df = pd.DataFrame({
+    'name': ['Alice', 'Bob', 'Charlie', 'Alice', 'Bob'],
+    'age': [21, 22, 23, 21, 22],
+    'score': [33, 45, 55, 33, 99]
 })
-df.set_index('Name', inplace=True)
-
-# Reading files
-dfcsv=pd.read_csv("Data Cleaning.csv")
-#print(dfcsv.head)
-
-
-dfexcel=pd.read_excel("Data Cleaning Start.xlsx", sheet_name="Data")
-#print(dfexcel.shape)
-#print(dfexcel.info)
-#print(dfexcel.head)
-#print(dfexcel.tail)
-#print(df.describe)
-print(df.columns)
-
-
+print(df)
