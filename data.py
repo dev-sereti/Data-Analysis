08564fdcb1a9ffd45cfec8e7d24b43461d4e3715
dev-sereti <devsereti@gmail.com>
2025-09-28 12:34:20 +0300

    columns

--- data.py
+++ data.py
@@ -14,9 +14,11 @@ dfcsv=pd.read_csv("Data Cleaning.csv")
 
 
 dfexcel=pd.read_excel("Data Cleaning Start.xlsx", sheet_name="Data")
-print(dfexcel.shape)
-print(dfexcel.info)
-print(dfexcel.head)
-print(dfexcel.tail)
-print(dfexcel.describe)
+#print(dfexcel.shape)
+#print(dfexcel.info)
+#print(dfexcel.head)
+#print(dfexcel.tail)
+#print(df.describe)
+print(df.columns)
+
 
