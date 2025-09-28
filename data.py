f7d38988678c3e2b9ee9e0a2719aa005824fa6e0
dev-sereti <devsereti@gmail.com>
2025-09-28 11:49:32 +0300

    value added to 2D TABLE

---
+++ data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data=pd.Series([10,20,30,40],index=['A','B','C','D'])
+df=pd.DataFrame({
+    'Name':['Alice','Bob','Charlie','Kelvin'],
+    'Age':[21,22,23,34],
+    'Score':[33,45,55,67]
+})
